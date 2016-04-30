6684b7f10ec7ce83d3b661b993cbf1c417c458d0
Jeff Hedfors <git.j@hedfors.net>
2016-04-30 14:15:27 -0700

    added db access

--- index.php
+++ index.php
@@ -191,6 +191,31 @@ if (defined('ENVIRONMENT'))
 		define('APPPATH', BASEPATH.$application_folder.'/');
 	}
 
+	//added for Heroku to connect to database
+	$dbopts = parse_url(getenv('DATABASE_URL'));
+	$app->register(new Herrera\Pdo\PdoServiceProvider(),
+	               array(
+	                   'pdo.dsn' => 'pgsql:dbname='.ltrim($dbopts["path"],'/').';host='.$dbopts["host"] . ';port=' . $dbopts["port"],
+	                   'pdo.username' => $dbopts["user"],
+	                   'pdo.password' => $dbopts["pass"]
+	               )
+	);
+	$app->get('/db/', function() use($app) {
+	  $st = $app['pdo']->prepare('SELECT name FROM test_table');
+	  $st->execute();
+
+	  $names = array();
+	  while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+	    $app['monolog']->addDebug('Row ' . $row['name']);
+	    $names[] = $row;
+	  }
+
+	  return $app['twig']->render('database.twig', array(
+	    'names' => $names
+	  ));
+	});
+
+
 /*
  * --------------------------------------------------------------------
  * LOAD THE BOOTSTRAP FILE
